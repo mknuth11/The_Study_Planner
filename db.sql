5c52996c9933e7f54c3582a3defe5ce7bfd5c544
mknuth11 <232231183+mknuth11@users.noreply.github.com>
2026-04-03 21:00:36 +0500

    UA1

--- db.sql
+++ db.sql
@@ -2,10 +2,12 @@ CREATE DATABASE study_planner;
 USE study_planner;
 
 
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-
-
-
-
-
-
+commit
