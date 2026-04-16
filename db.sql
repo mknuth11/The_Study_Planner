3c321904eb6a391921a03a6185cfef9fd87fbca5
mknuth11 <232231183+mknuth11@users.noreply.github.com>
2026-04-16 11:47:37 +0500

    te2

--- db.sql
+++ db.sql
@@ -10,4 +10,19 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    course_name VARCHAR(100) NOT NULL,
+    task_name VARCHAR(150) NOT NULL,
+    task_type ENUM('Homework', 'Quiz', 'Test', 'Project') NOT NULL,
+    deadline DATETIME NOT NULL,
+    estimated_time DECIMAL(4,1) NOT NULL,
+    notes TEXT,
+    is_completed BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
 commit
